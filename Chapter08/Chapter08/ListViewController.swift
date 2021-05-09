330e34c4dde63689bb10ae9f2472c508309b0898
helloworldjay <sj.seungjin.baek@gmail.com>
2021-05-09 21:57:12 +0900

    [#9]feat: 8.2.1 MyMovieChart 구현

---
+++ Chapter08/Chapter08/ListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ListViewController.swift
+//  Chapter08
+//
+//  Created by Seungjin Baek on 2021/05/09.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+}
---
+++ Chapter08/Chapter08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter08
+//
+//  Created by Seungjin Baek on 2021/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
