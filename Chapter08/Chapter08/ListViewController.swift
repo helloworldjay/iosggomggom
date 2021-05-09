163c0a43f1bc9bc9b35083de59539d7680e92031
helloworldjay <sj.seungjin.baek@gmail.com>
2021-05-09 22:54:08 +0900

    [#9]refactor: 8.3.1 리팩토링(p.513)

--- Chapter08/Chapter08/ListViewController.swift
+++ Chapter08/Chapter08/ListViewController.swift
@@ -8,32 +8,25 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-    var list = [MovieVO]()
     
-    override func viewDidLoad() {
-        var mvo = MovieVO()
-        mvo.title = "다크나이트"
-        mvo.description = "영웅물에 철학에 음악까지 더해져 예술이 되다."
-        mvo.opendate = "2008-09-04"
-        mvo.rating = 8.95
-        
-        self.list.append(mvo)
-        
-        mvo = MovieVO()
-        mvo.title = "호우시절"
-        mvo.description = "때를 알고 내리는 좋은 비"
-        mvo.opendate = "2009-10-08"
-        mvo.rating = 7.31
-        
-        self.list.append(mvo)
-        
-        mvo = MovieVO()
-        mvo.title = "말할 수 없는 비밀"
-        mvo.description = "여기서 너까지 다섯 걸음"
-        mvo.opendate = "2015-05-07"
-        mvo.rating = 9.19
-        
-        self.list.append(mvo)
-        
-    }
+    var dataset = [
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+    ]
+    
+    
+    lazy var list: [MovieVO] = {
+        var datalist = [MovieVO]()
+        for (title, desc, opendate, rating) in self.dataset {
+            let mvo = MovieVO()
+            mvo.title = title
+            mvo.description = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+            
+            datalist.append(mvo)
+        }
+        return datalist
+    }()
 }
