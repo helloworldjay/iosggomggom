0bee14f45b5fce2e2c1c75f57247e9872d34293c
helloworldjay <sj.seungjin.baek@gmail.com>
2021-05-09 22:14:22 +0900

    [#9]feat: 8.3.1 데이터 소스 만들기

--- Chapter08/Chapter08/ListViewController.swift
+++ Chapter08/Chapter08/ListViewController.swift
@@ -8,5 +8,32 @@
 import UIKit
 
 class ListViewController: UITableViewController {
+    var list = [MovieVO]()
     
+    override func viewDidLoad() {
+        var mvo = MovieVO()
+        mvo.title = "다크나이트"
+        mvo.description = "영웅물에 철학에 음악까지 더해져 예술이 되다."
+        mvo.opendate = "2008-09-04"
+        mvo.rating = 8.95
+        
+        self.list.append(mvo)
+        
+        mvo = MovieVO()
+        mvo.title = "호우시절"
+        mvo.description = "때를 알고 내리는 좋은 비"
+        mvo.opendate = "2009-10-08"
+        mvo.rating = 7.31
+        
+        self.list.append(mvo)
+        
+        mvo = MovieVO()
+        mvo.title = "말할 수 없는 비밀"
+        mvo.description = "여기서 너까지 다섯 걸음"
+        mvo.opendate = "2015-05-07"
+        mvo.rating = 9.19
+        
+        self.list.append(mvo)
+        
+    }
 }
---
+++ Chapter08/Chapter08/MovieVO.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVO.swift
+//  Chapter08
+//
+//  Created by Seungjin Baek on 2021/05/09.
+//
+
+import Foundation
+
+class MovieVO {
+    var thumbnail: String?
+    var title: String?
+    var description: String?
+    var detail: String?
+    var opendate: String?
+    var rating: Double?
+}
